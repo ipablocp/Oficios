108e9217983613e8e3d29c6e5f1bd20465a9abb5
Pablo <ipablocp@gmail.com>
2014-04-11 20:36:35 +0100

    Initial commit

---
+++ Oficios/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Oficios
+//
+//  Created by Pablo * on 11/04/14.
+//  Copyright (c) 2014 DSIC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ OficiosTests/OficiosTests.m
@@ -0,0 +1,34 @@
+//
+//  OficiosTests.m
+//  OficiosTests
+//
+//  Created by Pablo * on 11/04/14.
+//  Copyright (c) 2014 DSIC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface OficiosTests : XCTestCase
+
+@end
+
+@implementation OficiosTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
